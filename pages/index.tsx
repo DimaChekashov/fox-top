1a13bc032e2f23e191addc8edce7a6fbc05ec8e0
DimaChekashov <foxdima99@gmail.com>
2022-01-02 17:59:19 +0300

    add classnames Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { Htag } from "../components";
+import { Button } from "../components/Button/Button";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h2">Text</Htag>
+      <Button appearance="primary">Button 1</Button>
+      <Button appearance="ghost">Button 2</Button>
     </div>
   );
 }
