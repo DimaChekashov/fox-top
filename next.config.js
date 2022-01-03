897455687176be73c274e11102a4a9bb4bc15177
DimaChekashov <foxdima99@gmail.com>
2022-01-03 22:20:09 +0300

    fix config svgr

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ module.exports = {
     webpack: (config, options) => {
         config.module.rules.push({
             test: /\.svg$/,
-            issuer: {
-                test: /\.(js|ts)x?$/,
-            },
-            use: ["@svgr/webpack", "url-loader"],
+            issuer: { and: [/\.(js|ts)x?$/] },
+            use: ["@svgr/webpack"],
         });
 
         return config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home(): JSX.Element {
       <Button appearance="primary">Button 1</Button>
       <Button appearance="ghost">Button 2</Button>
       <Button appearance="primary" arrow="right">Button arrow</Button>
+      <Button appearance="ghost" arrow="down">Button arrow down</Button>
     </div>
   );
 }
