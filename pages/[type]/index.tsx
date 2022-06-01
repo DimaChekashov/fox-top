209c3cfb1f847c8b8f69d52ccd37bb8f51841a07
DimaChekashov <foxdima99@gmail.com>
2022-06-01 10:26:50 +0300

    implement sent form

--- pages/[type]/index.tsx
+++ pages/[type]/index.tsx
@@ -5,6 +5,7 @@ import { MenuItem } from "../../interfaces/menu.interface";
 import { withLayout } from "../../layout/Layout";
 import { firstLevelMenu } from "../../helpers/helpers";
 import { ParsedUrlQuery } from "node:querystring";
+import { API } from "../../helpers/api";
 
 function Type({ firstCategory }: TypeProps): JSX.Element {
 
@@ -28,15 +29,15 @@ export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }: GetS
     if(!params) {
         return {
             notFound: true
-        }
+        };
     }
     const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type);
     if(!firstCategoryItem) {
         return {
             notFound: true
-        }
+        };
     }
-    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
         firstCategory: firstCategoryItem.id
     });
     return {
@@ -44,8 +45,8 @@ export const getStaticProps: GetStaticProps<TypeProps> = async ({ params }: GetS
         menu,
         firstCategory: firstCategoryItem.id
         }
-    }
-}
+    };
+};
 
 interface TypeProps extends Record<string, unknown> {
     menu: MenuItem[];
