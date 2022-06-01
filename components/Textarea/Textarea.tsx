880596e61abddb28c71fe97002f302ed98203af6
DimaChekashov <foxdima99@gmail.com>
2022-06-01 09:31:31 +0300

    add validation to ReviewForm

--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -4,8 +4,17 @@ import cn from 'classnames';
 import { ForwardedRef, forwardRef } from "react";
 
 
-export const Textarea = forwardRef(({className, ...props}: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
+export const Textarea = forwardRef(({className, error, ...props}: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
     return (
-        <textarea className={cn(className, styles.textarea)} ref={ref} {...props} />
+        <div className={cn(className, styles.textareaWrapper)}>
+            <textarea 
+                className={cn(styles.textarea, {
+                    [styles.error]: error
+                })} 
+                ref={ref} 
+                {...props} 
+            />
+            {error && <span className={styles.errorMessage}>{error.message}</span>}
+        </div>
     );
 });
