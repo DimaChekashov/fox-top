f51e76f8804e04e47734b03f5d091d5ae89957f5
DimaChekashov <foxdima99@gmail.com>
2022-06-06 12:40:09 +0300

    add hover btn

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,14 +2,17 @@ import { ButtonProps } from "./Button.props";
 import styles from './Button.module.css';
 import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
+import { motion } from "framer-motion";
 
 export const Button = ({children, arrow = 'none', appearance, className, ...props}: ButtonProps): JSX.Element => {
     return (
-        <button className={cn(styles.button, className, {
-            [styles.primary]: appearance == 'primary',
-            [styles.ghost]: appearance == 'ghost'
-        })}
-        {...props}
+        <motion.button 
+            whileHover={{ scale: 1.05}}
+            className={cn(styles.button, className, {
+                [styles.primary]: appearance == 'primary',
+                [styles.ghost]: appearance == 'ghost'
+            })}
+            {...props}
         >
             {children}
             {arrow !== 'none' && <span className={cn(styles.arrow, {
@@ -17,6 +20,6 @@ export const Button = ({children, arrow = 'none', appearance, className, ...prop
             })}>
                 <ArrowIcon/>
                 </span>}
-        </button>
-    )
+        </motion.button>
+    );
 };
